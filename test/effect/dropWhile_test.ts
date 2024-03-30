d5602b343b4052d081f8a212bbe675afcf5c66d7
airmanxtw <airmanx@stust.edu.tw>
2024-03-30 20:17:03 +0800

    drop Negative

--- test/effect/dropWhile_test.ts
+++ test/effect/dropWhile_test.ts
@@ -3,14 +3,14 @@ import { Effect, Types } from "npm:effect";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1];
 
-const drop1 = Effect.dropWhile(numbers, (n: number) => Effect.succeed(n > 0));
-const prog1 = Effect.runSync(drop1);
+const dropNegative1 = Effect.dropWhile(numbers, (n: number) => Effect.succeed(n > 0));
+const prog1 = Effect.runSync(dropNegative1);
 Deno.test("Effect.dropWhile 1", () => {
   assertEquals(prog1, [-1]);
 });
 
-const drop2 = Effect.dropWhile<number, never, never>((n) => Effect.succeed(n > 0));
-const prog2 = Effect.runSync(drop2(numbers));
+const dropNegative2 = Effect.dropWhile<number, never, never>((n) => Effect.succeed(n > 0));
+const prog2 = Effect.runSync(dropNegative2(numbers));
 
 Deno.test("Effect.dropWhile 2", () => {
   assertEquals(prog2, [-1]);
