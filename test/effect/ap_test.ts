b7b3da914cadca4d2ecda359db5d9e0ccd7c5c45
airmanxtw <airmanx@stust.edu.tw>
2024-03-30 11:21:40 +0800

    update

--- test/effect/ap_test.ts
+++ test/effect/ap_test.ts
@@ -1,17 +1,15 @@
 import { assertEquals } from "assert";
-import { Effect, Exit } from "npm:effect";
+import { Effect } from "npm:effect";
 
 const add = (x: number, y: number): Effect.Effect<number, string> =>
   x > 0 && y > 0 ? Effect.succeed(x + y) : Effect.fail("x and y must be greater than 0");
 
-const addOne = Effect.fail((x: number) => `Error: ${x}`);
-const test = Effect.ap(add(5, 6))(addOne);
-const prog = Effect.runSyncExit(test);
+const addOne = (x: number): Effect.Effect<(a: number) => number, number> =>
+  x > 5 ? Effect.succeed((y: number) => y + 1) : Effect.fail(0);
+
+const test = Effect.ap(add(5, 6))(addOne(100));
+const prog = Effect.runSync(test);
 
 Deno.test("Effect.ap", () => {
-  const result = Exit.match(prog, {
-    onSuccess: (x) => x,
-    onFailure: (_) => "error",
-  });
-  assertEquals(result, "error");
+  assertEquals(prog, 12);
 });
