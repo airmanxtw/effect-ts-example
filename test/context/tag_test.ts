e041dab2e250a3b7fe70d05daa4bab3a0faf2a77
airmanxtw <airmanx@stust.edu.tw>
2024-04-06 22:35:02 +0800

    context tag

---
+++ test/context/tag_test.ts
@@ -0,0 +1,27 @@
+import { assertEquals } from "assert";
+import { Effect, Context } from "npm:effect";
+
+class Pi extends Context.Tag("Pi")<
+  Pi,
+  {
+    readonly value: Effect.Effect<number>;
+    readonly version: Effect.Effect<string>;
+  }
+>() {}
+
+const prog = (r: number) =>
+  Pi.pipe(
+    Effect.flatMap((pi) => pi.value),
+    Effect.map((pivalue) => pivalue * r * r)
+  );
+
+const progrunable = Effect.provideService(prog(10), Pi, {
+  value: Effect.succeed(3.14),
+  version: Effect.succeed("v1"),
+});
+
+const result = Effect.runSync(progrunable);
+
+Deno.test("Context.tag", () => {
+  assertEquals(result, 314);
+});
