52c1ffe4b46cfda2abc116a302805833788e55fb
airmanxtw <airmanx@stust.edu.tw>
2024-05-15 15:02:08 +0800

    update ap

--- test/effect/ap_test.ts
+++ test/effect/ap_test.ts
@@ -1,13 +1,13 @@
 import { assertEquals } from "assert";
+import { never } from "npm:effect/Fiber";
 import { Effect } from "npm:effect";
 
 const add = (x: number, y: number): Effect.Effect<number, string> =>
   x > 0 && y > 0 ? Effect.succeed(x + y) : Effect.fail("x and y must be greater than 0");
 
-const addOne = (x: number): Effect.Effect<(a: number) => number, number> =>
-  x > 5 ? Effect.succeed((y: number) => y + 1) : Effect.fail(0);
+const addOne = Effect.succeed((x: number) => x + 1);
 
-const test = Effect.ap(add(5, 6))(addOne(100));
+const test = Effect.ap(add(5, 6))(addOne);
 const prog = Effect.runSync(test);
 
 Deno.test("Effect.ap", () => {
