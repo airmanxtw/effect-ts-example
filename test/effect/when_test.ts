7db9eb7f6b9663a10c22a2598641b089590c9f95
airmanxtw <airmanx@stust.edu.tw>
2024-05-17 11:43:54 +0800

    add when example

--- test/effect/when_test.ts
+++ test/effect/when_test.ts
@@ -1,2 +1,16 @@
-import { Effect } from "npm:effect";
+import { Effect, Exit, Option } from "npm:effect";
+import { assertEquals } from "assert";
 const result = (n: number) => Effect.succeed(n).pipe(Effect.when(() => n > 10));
+
+const prog = Effect.runSync(result(15));
+
+Deno.test("Effect.when", () => {
+  const result=Option.match(prog,{
+    onSome:(v)=>v,
+    onNone:()=>0
+  });
+  assertEquals(result, 15);
+});
+
+
+
