9e70cec524145363df27149e755920809cc6c98d
airmanxtw <airmanx@stust.edu.tw>
2024-05-28 16:50:53 +0800

    add zip example

---
+++ test/effect/zipLeft_test.ts
@@ -0,0 +1,13 @@
+import { assertEquals } from "assert";
+import { Effect, pipe } from "npm:effect";
+
+const A1 = Effect.succeed(1);
+const A2 = Effect.succeed(2);
+
+const prog = pipe(A1, Effect.zipLeft(A2));
+
+const result = Effect.runSync(prog);
+
+Deno.test("Effect.zip", () => {
+  assertEquals(result, 1);
+});
