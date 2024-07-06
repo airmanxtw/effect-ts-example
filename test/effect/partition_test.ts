1503c0711c84fcf6fa4fa1a594791f59a2457e45
airmanxtw <airmanx@stust.edu.tw>
2024-07-06 20:25:50 +0800

    feat: Add Effect.partition test

---
+++ test/effect/partition_test.ts
@@ -0,0 +1,13 @@
+import { assertEquals } from "assert";
+import { Effect, ReadonlyArray } from "npm:effect";
+
+const fun1 = (a: number): Effect.Effect<number, string> =>
+  a > 10 ? Effect.fail("a is greater than 10") : Effect.succeed(a);
+
+const prog = Effect.partition(ReadonlyArray.range(1, 20), fun1);
+
+const [_, success] = Effect.runSync(prog);
+
+Deno.test("Effect.partition", () => {
+  assertEquals(success, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
