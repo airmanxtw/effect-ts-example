e7bf757a64c9fcb7ad7206fb466d47610047e25b
airmanxtw <airmanx@stust.edu.tw>
2025-01-10 00:56:42 +0800

    feat: Update YCombinator_test to use fun2 for addition example

--- test/math/YCombinator_test.ts
+++ test/math/YCombinator_test.ts
@@ -4,9 +4,7 @@ import { assertEquals } from "assert";
 
 const fun2 = (f: (x: number) => number) => (y: number) => f(y);
 
-//const result = fun2((x) => (x < 2 ? 1 : x * fun(x - 1)))(3);
-
 Deno.test("example1", () => {
   //assertEquals(fun(3), 6);
-  assertEquals(fun2((x) => (x < 2 ? 1 : x * fun(x - 1)))(3), 6);
+  assertEquals(fun2((x) => x + 1)(2), 3);
 });
