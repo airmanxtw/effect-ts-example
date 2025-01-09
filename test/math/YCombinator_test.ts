eb35f2bf50773634af41d5ce019cffcb73e5ebf1
airmanxtw <airmanx@stust.edu.tw>
2025-01-10 00:32:28 +0800

    feat: Update YCombinator_test to use fun2 for factorial calculation

--- test/math/YCombinator_test.ts
+++ test/math/YCombinator_test.ts
@@ -1,7 +1,12 @@
 import { assertEquals } from "assert";
 
-const fun = (x: number): number => (x < 2 ? 1 : x * fun(x - 1));
+//const fun = (x: number): number => (x < 2 ? 1 : x * fun(x - 1));
+
+const fun2 = (f: (x: number) => number) => (y: number) => f(y);
+
+//const result = fun2((x) => (x < 2 ? 1 : x * fun(x - 1)))(3);
 
 Deno.test("example1", () => {
-  assertEquals(fun(3), 6);
+  //assertEquals(fun(3), 6);
+  assertEquals(fun2((x) => (x < 2 ? 1 : x * fun(x - 1)))(3), 6);
 });
